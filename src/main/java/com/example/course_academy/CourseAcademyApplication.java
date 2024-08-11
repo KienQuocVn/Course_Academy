e125fe969760d086190a50a0ca6377ac3a307834
KienQuoc <kieukienquocvn@gmail.com>
2024-08-11 12:12:52 +0700

    firstcommit

---
+++ src/main/java/com/example/course_academy/CourseAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.example.course_academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/course_academy/CourseAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.course_academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
